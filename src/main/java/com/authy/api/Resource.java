bf0d52da5c726f5d923f0521d49283d9ca8a180d
Moises Vargas M <moisesweb@gmail.com>
2015-11-17 08:20:09 -0500

    Fix identation resource

--- src/main/java/com/authy/api/Resource.java
+++ src/main/java/com/authy/api/Resource.java
@@ -24,225 +24,225 @@ import com.authy.AuthyApiClient;
  *
  */
 public class Resource {
-	private String apiUri, apiKey;
-	private int status;
-	private boolean testFlag = false;
+  private String apiUri, apiKey;
+  private int status;
+  private boolean testFlag = false;
   private Map<String, String> defaultOptions;
 
-	public static final String ENCODE = "UTF-8";
-	public static final String XML_CONTENT_TYPE = "application/xml";
-	public static final String JSON_CONTENT_TYPE = "application/json";
+  public static final String ENCODE = "UTF-8";
+  public static final String XML_CONTENT_TYPE = "application/xml";
+  public static final String JSON_CONTENT_TYPE = "application/json";
 
-	private boolean isJSON = false;
-	private String contentType = XML_CONTENT_TYPE;
-	
-	public Resource(String uri, String key) {
-		apiUri = uri;
-		apiKey = key;
-	}
+  private boolean isJSON = false;
+  private String contentType = XML_CONTENT_TYPE;
 
-	public Resource(String uri, String key, String contentType) {
-		apiUri = uri;
-		apiKey = key;
-		isJSON = contentType == "JSON";
-		if(isJSON)
-			this.contentType = JSON_CONTENT_TYPE;
-	}
+  public Resource(String uri, String key) {
+    apiUri = uri;
+    apiKey = key;
+  }
 
-	public Resource(String uri, String key, boolean testFlag) {
-		apiUri = uri;
-		apiKey = key;
-		this.testFlag = testFlag;
-	}
+  public Resource(String uri, String key, String contentType) {
+    apiUri = uri;
+    apiKey = key;
+    isJSON = contentType == "JSON";
+    if(isJSON)
+      this.contentType = JSON_CONTENT_TYPE;
+  }
 
-	public Resource(String uri, String key, boolean testFlag,  String contentType) {
-		apiUri = uri;
-		apiKey = key;
-		this.testFlag = testFlag;
-		isJSON = contentType == "JSON";
-		if(isJSON)
-			this.contentType = JSON_CONTENT_TYPE;
-	}
+  public Resource(String uri, String key, boolean testFlag) {
+    apiUri = uri;
+    apiKey = key;
+    this.testFlag = testFlag;
+  }
 
-	/**
-	 * POST method.
-	 * @param path
-	 * @param data
-	 * @return response from API.
-	 */
-	public String post(String path, Response data) {
-		return request("POST", path, data, getDefaultOptions());
-	}
-	
-	/**
-	 * GET method.
-	 * @param path
-	 * @param data
-	 * @return response from API.
-	 */
-	public String get(String path, Response data) {
-		return request("GET", path, data, getDefaultOptions());
-	}
-	
-	/**
-	 * PUT method.
-	 * @param path
-	 * @param data
-	 * @return response from API.
-	 */
-	public String put(String path, Response data) {
-		return request("PUT", path, data, getDefaultOptions());
-	}
-	
-	/**
-	 * DELETE method.
-	 * @param path
-	 * @param data
-	 * @return response from API.
-	 */
-	public String delete(String path, Response data) {
-		return request("DELETE", path, data, getDefaultOptions());
-	}
-	
-	public String request(String method, String path, Response data, Map<String, String> options) {
-		HttpURLConnection connection = null;
-		String answer = null;
-		
-		try {
-			StringBuffer sb = new StringBuffer();
-			sb.append("?api_key=" + apiKey);
-			
-			if(method.equals("GET")) {
-				sb.append(prepareGet(data));
-			}
-			
-			URL url = new URL(apiUri + path + sb.toString());
-			connection = createConnection(url, method, options);
-			
-			if(method.equals("POST") || method.equals("PUT")) {
-				if(isJSON) {
-					writeJson(connection, data);
-				} else {
-					writeXml(connection, data);
-				}
-			}
+  public Resource(String uri, String key, boolean testFlag,  String contentType) {
+    apiUri = uri;
+    apiKey = key;
+    this.testFlag = testFlag;
+    isJSON = contentType == "JSON";
+    if(isJSON)
+      this.contentType = JSON_CONTENT_TYPE;
+  }
 
-			status = connection.getResponseCode();
-			answer = getResponse(connection);
-		}
-		catch(SSLHandshakeException e) {
-			System.err.println("SSL verification is failing. This might be because of an attack. Contact support@authy.com");
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		return answer;
-	}
+  /**
+   * POST method.
+   * @param path
+   * @param data
+   * @return response from API.
+   */
+  public String post(String path, Response data) {
+    return request("POST", path, data, getDefaultOptions());
+  }
 
-	/**
-	 * Get status from response.
-	 * @return status code.
-	 */
-	public int getStatus() {
-		return status;
-	}
-	
-	protected HttpURLConnection createConnection(URL url, String method, 
-			Map<String, String> options) throws Exception {
+  /**
+   * GET method.
+   * @param path
+   * @param data
+   * @return response from API.
+   */
+  public String get(String path, Response data) {
+    return request("GET", path, data, getDefaultOptions());
+  }
 
-		
-		HttpURLConnection connection = null;
-		if(testFlag)
-			connection = (HttpURLConnection)url.openConnection();
-		else
-			connection = (HttpsURLConnection)url.openConnection();
-		
-		connection.setRequestMethod(method);
-		
-		for(Entry<String, String> s : options.entrySet()) {
-			connection.setRequestProperty(s.getKey(), s.getValue());
-		}
+  /**
+   * PUT method.
+   * @param path
+   * @param data
+   * @return response from API.
+   */
+  public String put(String path, Response data) {
+    return request("PUT", path, data, getDefaultOptions());
+  }
 
-		connection.setDoOutput(true);
-		
-		return connection;
-	}
-	
-	private String getResponse(HttpURLConnection connection) throws Exception {
-		InputStream in = null;
-		// Load stream
-		if(status != 200) {
-			in = connection.getErrorStream();
-		}
-		else {
-			in = connection.getInputStream();
-		}
-		
-		BufferedInputStream input = new BufferedInputStream(in);
-		StringBuffer sb = new StringBuffer();
-		int ch;
-		
-		while((ch = input.read()) != -1) {
-			sb.append((char)ch);
-		}
-		input.close();
+  /**
+   * DELETE method.
+   * @param path
+   * @param data
+   * @return response from API.
+   */
+  public String delete(String path, Response data) {
+    return request("DELETE", path, data, getDefaultOptions());
+  }
 
-		return sb.toString();
-	}
-	
-	private void writeXml(HttpURLConnection connection, Response data) throws SSLHandshakeException, IOException {
-		if(data == null)
-			return;
-		
-		OutputStream os = connection.getOutputStream();
-		
-		BufferedWriter output = new BufferedWriter(new OutputStreamWriter(os));
-		output.write(data.toXML());
-		output.flush();
-		output.close();
-	}
-	
-	private void writeJson(HttpURLConnection connection, Response data) throws SSLHandshakeException, IOException {
-		if(data == null)
-			return;
-		
-		OutputStream os = connection.getOutputStream();
-		
-		BufferedWriter output = new BufferedWriter(new OutputStreamWriter(os));
-		output.write(data.toJSON());
-		output.flush();
-		output.close();
-	}
-	
+  public String request(String method, String path, Response data, Map<String, String> options) {
+    HttpURLConnection connection = null;
+    String answer = null;
 
-	private String prepareGet(Response data) throws Exception {
-		if(data == null)
-			return "";
-		
-		StringBuffer sb = new StringBuffer();
-		Map<String, String> params = data.toMap();
+    try {
+      StringBuffer sb = new StringBuffer();
+      sb.append("?api_key=" + apiKey);
 
-		for(Entry<String, String> s : params.entrySet()) {
-			sb.append('&');
-			sb.append(URLEncoder.encode(s.getKey(), ENCODE) + "=" + URLEncoder.encode(s.getValue(), ENCODE));
-		}
-		
-		return sb.toString();
-	}
+      if(method.equals("GET")) {
+        sb.append(prepareGet(data));
+      }
 
-    private Map<String, String> getDefaultOptions() {
-        if(this.defaultOptions == null || this.defaultOptions.isEmpty()){
-            this.defaultOptions = new HashMap<String, String>();
-            this.defaultOptions.put("Content-Type", contentType);
-            this.defaultOptions.put("Content-Type", contentType);
-            this.defaultOptions.put("User-Agent", getUserAgent());
+      URL url = new URL(apiUri + path + sb.toString());
+      connection = createConnection(url, method, options);
+
+      if(method.equals("POST") || method.equals("PUT")) {
+        if(isJSON) {
+          writeJson(connection, data);
+        } else {
+          writeXml(connection, data);
         }
-        return this.defaultOptions;
+      }
+
+      status = connection.getResponseCode();
+      answer = getResponse(connection);
+    }
+    catch(SSLHandshakeException e) {
+      System.err.println("SSL verification is failing. This might be because of an attack. Contact support@authy.com");
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+    }
+    return answer;
+  }
+
+  /**
+   * Get status from response.
+   * @return status code.
+   */
+  public int getStatus() {
+    return status;
+  }
+
+  protected HttpURLConnection createConnection(URL url, String method,
+      Map<String, String> options) throws Exception {
+
+
+    HttpURLConnection connection = null;
+    if(testFlag)
+      connection = (HttpURLConnection)url.openConnection();
+    else
+      connection = (HttpsURLConnection)url.openConnection();
+
+    connection.setRequestMethod(method);
+
+    for(Entry<String, String> s : options.entrySet()) {
+      connection.setRequestProperty(s.getKey(), s.getValue());
     }
 
-    private String getUserAgent() {
-        String os = String.format("%s-%s-%s; Java %s", System.getProperty("os.name"), System.getProperty("os.version"),
-                System.getProperty("os.arch"), System.getProperty("java.specification.version"));
-        return String.format("%s/%s (%s)", AuthyApiClient.CLIENT_NAME, AuthyApiClient.VERSION, os);
+    connection.setDoOutput(true);
+
+    return connection;
+  }
+
+  private String getResponse(HttpURLConnection connection) throws Exception {
+    InputStream in = null;
+    // Load stream
+    if(status != 200) {
+      in = connection.getErrorStream();
     }
+    else {
+      in = connection.getInputStream();
+    }
+
+    BufferedInputStream input = new BufferedInputStream(in);
+    StringBuffer sb = new StringBuffer();
+    int ch;
+
+    while((ch = input.read()) != -1) {
+      sb.append((char)ch);
+    }
+    input.close();
+
+    return sb.toString();
+  }
+
+  private void writeXml(HttpURLConnection connection, Response data) throws SSLHandshakeException, IOException {
+    if(data == null)
+      return;
+
+    OutputStream os = connection.getOutputStream();
+
+    BufferedWriter output = new BufferedWriter(new OutputStreamWriter(os));
+    output.write(data.toXML());
+    output.flush();
+    output.close();
+  }
+
+  private void writeJson(HttpURLConnection connection, Response data) throws SSLHandshakeException, IOException {
+    if(data == null)
+      return;
+
+    OutputStream os = connection.getOutputStream();
+
+    BufferedWriter output = new BufferedWriter(new OutputStreamWriter(os));
+    output.write(data.toJSON());
+    output.flush();
+    output.close();
+  }
+
+
+  private String prepareGet(Response data) throws Exception {
+    if(data == null)
+      return "";
+
+    StringBuffer sb = new StringBuffer();
+    Map<String, String> params = data.toMap();
+
+    for(Entry<String, String> s : params.entrySet()) {
+      sb.append('&');
+      sb.append(URLEncoder.encode(s.getKey(), ENCODE) + "=" + URLEncoder.encode(s.getValue(), ENCODE));
+    }
+
+    return sb.toString();
+  }
+
+  private Map<String, String> getDefaultOptions() {
+    if(this.defaultOptions == null || this.defaultOptions.isEmpty()){
+      this.defaultOptions = new HashMap<String, String>();
+      this.defaultOptions.put("Content-Type", contentType);
+      this.defaultOptions.put("Content-Type", contentType);
+      this.defaultOptions.put("User-Agent", getUserAgent());
+    }
+    return this.defaultOptions;
+  }
+
+  private String getUserAgent() {
+    String os = String.format("%s-%s-%s; Java %s", System.getProperty("os.name"), System.getProperty("os.version"),
+        System.getProperty("os.arch"), System.getProperty("java.specification.version"));
+    return String.format("%s/%s (%s)", AuthyApiClient.CLIENT_NAME, AuthyApiClient.VERSION, os);
+  }
 }
