5b29d64af94d931ac6f155ee1c4c470703e0183b
Moises Vargas M <moisesweb@gmail.com>
2017-05-17 15:48:16 -0500

    bump up version

--- src/main/java/com/authy/AuthyApiClient.java
+++ src/main/java/com/authy/AuthyApiClient.java
@@ -10,7 +10,7 @@ import com.authy.api.*;
 public class AuthyApiClient {
     public static final String CLIENT_NAME = "AuthyJava";
     public static final String DEFAULT_API_URI = "https://api.authy.com";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
     private Users users;
     private Tokens tokens;
     private String apiUri, apiKey;
