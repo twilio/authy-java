b73fe7c54eb1cabc12678a3849b0ed5466ca9714
Julian Suarez <jsuarez@twilio.com>
2018-05-15 14:43:59 -0500

    Prepare release 1.3.2

--- src/main/java/com/authy/AuthyApiClient.java
+++ src/main/java/com/authy/AuthyApiClient.java
@@ -10,7 +10,7 @@ import com.authy.api.*;
 public class AuthyApiClient {
     public static final String CLIENT_NAME = "AuthyJava";
     public static final String DEFAULT_API_URI = "https://api.authy.com";
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
     private Users users;
     private Tokens tokens;
     private String apiUri, apiKey;
